d5e177254efb78aa32a4b8c90b4a98e459dcd05f
jodelajo <jodelajo@gmail.com>
2022-03-27 10:49:01 +0200

    delete friend function

--- src/components/friendsList/FriendsList.js
+++ src/components/friendsList/FriendsList.js
@@ -1,15 +1,55 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
-import { fetchFriends } from "../../features/friendsList";
+import {
+  fetchFriends,
+  selectFriend,
+  friendsLoading,
+  friendsFetched,
+  friendsFetchedFailed,
+  friendsUpdated,
+  friendDeleted,
+  deleteFriend,
+} from "../../features/friendsList";
 import "./FriendsList.css";
 
 function FriendsList() {
   const dispatch = useDispatch();
-  const { status, error, friends } = useSelector(
+  const { status, error, friends, selectedFriend } = useSelector(
     (state) => state.friendsList.value
   );
-  console.log("friendslist", friends);
+  // const [selectedFriend, setSelecteFriend] = useState();
+  const [newUserName, setNewUserName] = useState("");
+  // console.log("friendslist", friends);
+  // console.log("new username", newUserName);
+  // console.log("selected friend", selectedFriend);
+
+  // const updateFriends = async () => {
+  //   const myFriend = selectedFriend.friend;
+  //   console.log("!!!!myFriend", myFriend);
+  //   const friendId = selectedFriend.friend.id;
+  //   const patchUrl = `https://jsonplaceholder.typicode.com/users/${friendId}`;
+  //   console.log("patchurl", patchUrl);
+  //   dispatch(friendsLoading());
+  //   // dispatch(selectFriend());
+  //   try {
+  //     await axios.patch(
+  //       `https://jsonplaceholder.typicode.com/users/${friendId}`,
+  //       {
+  //         method: "PATCH",
+  //         body: { username: newUserName },
+  //         headers: {
+  //           "Content-type": "application/json; charset=UTF-8",
+  //         },
+  //       }
+  //     );
+  //     console.log("hoi");
+  //     // dispatch(friendsFetched(response.data));
+  //   } catch (error) {
+  //     dispatch(friendsFetchedFailed(error.message));
+  //   }
+  // };
 
   useEffect(() => {
     dispatch(fetchFriends());
@@ -22,19 +62,31 @@ function FriendsList() {
         FriendsList: {friends.length}{" "}
       </div>
       <div className="friendsSector">
-        {friends.map((friend) => {
+        {friends?.map((friend) => {
           return (
             <ul key={friend.id} className="">
-              <li className="friendslist">
+              <li
+                className="friendslist"
+                onClick={() => dispatch(selectFriend(friend))}
+              >
                 <h3>{friend.name}</h3>
                 <p className="username">{friend.username}</p>
                 <div className="change">
-                  <input type="text" placeholder={`Change username`} />
+                  <input
+                    type="text"
+                    placeholder={`Change username`}
+                    onChange={(e) => setNewUserName(e.target.value)}
+                  />
                   <button className="update">Update</button>
                 </div>
                 <div className="change">
                   <p className="hate">NOT A FRIEND OF MINE 😡 </p>
-                  <button className="destroy">Destroy⚡</button>
+                  <button
+                    className="destroy"
+                    onClick={() => dispatch(deleteFriend(friend.id))}
+                  >
+                    Destroy⚡
+                  </button>
                 </div>
               </li>
             </ul>
--- src/features/friendsList.js
+++ src/features/friendsList.js
@@ -1,7 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, current } from "@reduxjs/toolkit";
+import { useSelector } from "react-redux";
 import axios from "axios";
 
-const initialStateValue = { status: "idle", error: null, friends: [] };
+const initialStateValue = {
+  status: "idle",
+  error: null,
+  friends: [],
+  selectedFriend: {},
+};
 
 export const friendsListSlice = createSlice({
   name: "friendsList",
@@ -20,19 +26,78 @@ export const friendsListSlice = createSlice({
       state.value.error = action.payload;
       //   console.log("hanlde action here later");
     },
+    selectFriend: (state, action) => {
+      state.value.selectedFriend = action.payload;
+    },
+    friendDeleted: (state, action) => {
+      console.log("deleting my friend");
+      state.value.status = "success";
+      state.value.friends = state.value.friends.filter(
+        (friend) => action.payload !== friend.id
+      );
+    },
+    // friendsUpdated: (state, action) => {
+    //   const currentSelectedFriendState = current(
+    //     state.value.selectedFriend.friend
+    //   );
+    //   const currentUsername = current(
+    //     state.value.selectedFriend.friend.username
+    //   );
+    //   console.log("hoiiii", currentSelectedFriendState);
+    //   console.log("currentUsername", currentSelectedFriendState.username);
+    //   const { username, id } = action.payload;
+    //   // const existingFriend = state.find(
+    //   //   (selectedFriend) => selectedFriend.id === id
+    //   // );
+    //   // if (existingFriend) {
+    //   //   console.log("existing friend", existingFriend);
+    //   // }
+    //   // console.log("existing friend", existingFriend);
+    //   console.log("username", username);
+    //   console.log("id", id);
+    //   if (username) {
+    //     return {
+    //       currentSelectedFriendState: {
+    //         username: username,
+    //       },
+    //     };
+    //   }
+    // },
   },
 });
 
-export const { friendsLoading, friendsFetched, friendsFetchedFailed } =
-  friendsListSlice.actions;
+export const {
+  friendsLoading,
+  friendsFetched,
+  friendsFetchedFailed,
+  selectFriend,
+  friendDeleted,
+  // friendsUpdated,
+} = friendsListSlice.actions;
 
 export const fetchFriends = () => async (dispatch) => {
+  // const friendId = getState();
+  // console.log("friendid", friendId);
+
   dispatch(friendsLoading());
   try {
     const response = await axios.get(
       "https://jsonplaceholder.typicode.com/users"
     );
     dispatch(friendsFetched(response.data));
+  } catch (error) {
+    dispatch(friendsFetchedFailed(error.message));
+  }
+};
+
+export const deleteFriend = (id) => async (dispatch) => {
+  console.log("delete ", id);
+  dispatch(friendsLoading());
+  try {
+    await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+    console.log("in axios delete");
+    dispatch(friendDeleted(id));
+    // dispatch(friendsFetched(response.data));
   } catch (error) {
     dispatch(friendsFetchedFailed(error.message));
   }
